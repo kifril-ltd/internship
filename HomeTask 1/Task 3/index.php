f1fec77be954d0cc30ee2a2119bd34ffd3543707
Сухоруков Кирилл Олегович <sukhorukov.kirill23@gmail.com>
2021-04-17 18:42:22 +0300

    Добавил собственные реализации функций для продвинутых задач

--- HomeTask 1/Task 3/index.php
+++ HomeTask 1/Task 3/index.php
@@ -16,8 +16,60 @@ function getRandomArray(int $itemCount, int $min, int $max) : array {
     return $rand_array;
 }
 
+/**
+ * getArrayMinValue -- получение минимального элемента массива
+ *
+ * @param  array $array -- массив
+ * @return int
+ */
+function getArrayMinValue(array $array) : int {
+    $min = $array[0];
+    foreach ($array as $value) {
+        if ($value < $min) {
+            $min = $value;
+        }
+    }
+    return $min;
+}
+
+/**
+ * getArrayMaxValue -- получение максимального элемента массива
+ *
+ * @param  array $array -- массив
+ * @return int
+ */
+function getArrayMaxValue(array $array) : int {
+    $max = $array[0];
+    foreach ($array as $value) {
+        if ($value > $max) {
+            $max = $value;
+        }
+    }
+    return $max;
+}
+
+/**
+ * getArrayAvgValue -- получение среднего значения элементов массива
+ *
+ * @param  array $array -- массив
+ * @return float
+ */
+function getArrayAvgValue(array $array) : float {
+    $sum = 0;
+    foreach ($array as $value) {
+        $sum += $value;
+    }
+    return $sum / count($array);
+}
+
 $rand_array = getRandomArray(50, 0, 1000);
 
+echo "------------Собственные функции------------<br>";
+echo 'Минимальное значение: ' . getArrayMinValue($rand_array) . '<br>';
+echo 'Максимальное значение: ' . getArrayMaxValue($rand_array) . '<br>';
+echo 'Среднее значение: ' . getArrayAvgValue($rand_array) . '<br>';
+
+echo "------------Встроенные функции------------<br>";
 echo 'Минимальное значение: ' . min($rand_array) . '<br>';
 echo 'Максимальное значение: ' . max($rand_array) . '<br>';
 echo 'Среднее значение: ' . array_sum($rand_array) / count($rand_array) . '<br>';
--- HomeTask 1/Task 4/index.php
+++ HomeTask 1/Task 4/index.php
@@ -1,6 +1,23 @@
 <?php
 
+/**
+ * reverseString -- возвращает отзеркаленную строку
+ *
+ * @param  string $str -- строка
+ * @return string
+ */
+function reverseString(string $str) : string {
+    $revstr = '';
+    for ($i = 1; $i <= strlen($str); $i++) {
+        $revstr .= $str[-$i];
+    }
+    return $revstr;
+}
+
 // Проверка, что массив GET не пуст и что в нем есть ключ string
 if (!empty($_GET) && array_key_exists('string', $_GET)) {
+    echo "------------Собственная функция------------<br>";
+    echo reverseString($_GET['string']) . "<br>";
+    echo "------------Встроенная функция------------<br>";
     echo strrev($_GET['string']);
 }
